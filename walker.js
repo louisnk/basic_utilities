f918a38f8d650c299b4f972607dd326e12ea34ee
Louis <louis.kinley@gmail.com>
2014-08-23 15:28:02 -0700

    now takes optional param to provide extra functionality...more to be added

--- walker.js
+++ walker.js
@@ -1,38 +1,68 @@
-var fs = require('fs');
-var path = require('path');
 
-exports.findAll = function(dir, callback) {
+exports.findAll = function(dir, options, callback) {
+	if (typeof options === 'function') {
+		callback = options;
+		options = {};
+	}
 	
-	var templates = [];
+	var files = options.toJSON ? {} : [];
+	var dircount = 0;
 
 	function walk(directory, done) {
 
 		fs.readdir(directory, function(err, list) {
-			if (list.length > 0) {
-				list.forEach(function(file,i) {
-					var toRead = path.join(directory,file);
-					fs.stat(toRead, function(err, fileInfo) {
-						if (fileInfo.isDirectory()) {
-							walk(toRead, done);
-						} else if (fileInfo.isFile()) {
-							templates.push(toRead);
-							if (i == list.length - 1) {
+			if (err) return done(err, 'Luke dirwalker thinks there is no such directory');
+			list.forEach(function(file,i) {
+				var toRead = path.join(directory,file);
+				fs.stat(toRead, function(err, stats) {
+					if (stats.isDirectory()) {
+						
+						if (options.toJSON) {
+							setFolder(toRead);
+							dircount++;
+						} 
+
+						walk(toRead, done);
+
+					} else if (stats.isFile()) {
+						
+						if (options.toJSON) {
+							var tree = toRead.split(/[\/\\]/),
+									parent = tree[tree.length - 2];
+							if (files[parent] instanceof Array) {
+								files[parent].push(toRead);
+							}
+						} 
+						else files.push(toRead);
+						if (options.toJSON) {
+							if (i === list.length - 1 && dircount === Object.keys(files).length) {
 								return done(null,true);
 							}
-						} else return done(err, false);
-					});
+						}
+						if (!options.toJSON && i === list.length - 1) {
+							return done(null,true);
+						}
+					} else return done(err, false);
 				});
-			} else {
-				return done(null, 'No files in directory');
-			}
+			});
 		});		
 	}
 
+	function setFolder(folder) {
+		var dir = folder.slice(folder.lastIndexOf('\\') + 1);
+		if (dir === 'images') return true;
+		else {
+			files[dir] = [];
+			if (files.dir) return true;
+		}
+	}
+
+	
 	dir = path.normalize(dir)
 	walk(dir, function(err, done) {
-		if (done) {
-			return callback(null, templates);
+		if (err) {
+			return callback(err, done);
 		} else 
-			return callback(err, "Luke directory walker failed!");
+			return callback(null, files);
 	});
 }
