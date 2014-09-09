9ea47a494f5fed0eee078d7ee323c05dced424e9
Louis <louis.kinley@gmail.com>
2014-09-09 00:01:45 -0700

    wrong directory

--- tmp/recursor.js
+++ tmp/recursor.js
@@ -1,48 +1,47 @@
 // var recurse = module.exports;
-var slash = (process.platform === 'win32') ? '\\' : '/';
+var s = (process.platform === 'win32') ? '\\' : '/';
+
 var fs = require('fs');
 
 var findAll = function() {
-  var dirs = 0,
-      depth = 0,
+  var wait = 0,
       pairs = [],
       ltls = [],
 
   read = function(path, level, callback) {
-    console.log(dirs);
+
+    function done() {
+      return (!wait && level < 10 && level > 1);
+    }
+
     fs.readdir(path, function(err,files) {
+
       files.forEach(function(file, i) {
         if (!file.match(/^\./)) {
-          fs.stat(path + slash + file, function(err, stats) {
-            if (stats.isDirectory()) {
-              dirs++;
-              read(path + slash + file, level++, callback);
-
-            } else if (file.match(/(readme.md)|(README.md)/)) {
-              var folder = getModuleName(path + slash + file);
-              // console.log(pairLtl(folder));
-              pairs.push([path + slash + file, pairLtl(folder)]);
+          fs.stat(path + s + file, function(err, stats) {
+            if (file.match(/(readme.md)|(README.md)/)) {
+              var folder = getModuleName(path);
+              pairs.push([path + s + file, pairLtl(folder)]);
+            } else if (stats.isDirectory() && level < 10) {
+              wait++;
+              read(path + s + file, level++, callback);
             }
           });
         }
-        if (i === files.length - 1) {
-          dirs--;
-          if (dirs === 0) {
-            return callback(null, pairs);
-          }
-        }
-
       })
+      
+      wait--;
+      
+      if (done()) {
+        console.log(pairs.length)
+        // return callback(null, pairs);
+      }
+
     })
   },
 
-  // I feel like this could probably be a regex, 
-  // but is a little beyond my regex ability.
   getModuleName = function(path) {
-    var a = path.slice(0, path.lastIndexOf(slash)),
-        b = a.slice(a.lastIndexOf(slash) + 1);
-
-    return b;
+    return path.slice(path.lastIndexOf(s) + 1);
   },
 
   findLtls = function(views, callback) {
@@ -69,11 +68,12 @@ var findAll = function() {
     return '';
   }
   
-  findLtls(process.cwd() + slash + 'views', function(err, done) {
-    if (!err) 
-      read(process.cwd(), depth, function(err, files) {
-      console.log(files);
-    });    
+  findLtls(process.cwd() + s + 'views', function(err, found) {
+    if (!err && ltls.length > 0) {
+      read(process.cwd(), 0, function(err, files) {
+        console.log(files);
+      });    
+    }
   });
 
 
